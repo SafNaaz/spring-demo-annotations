7522d8234c37dfecdd8d5a09b2a0c54601d39b02
Safnas <iamsafnas@gmail.com>
2020-07-14 01:39:19 +0530

    read random fortune from file

--- src/com/safnas/springdemo/TennisCoach.java
+++ src/com/safnas/springdemo/TennisCoach.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Component;
 @Component
 public class TennisCoach implements Coach {
 	@Autowired
-	@Qualifier("randomFortuneService")
+	@Qualifier("fileFortuneService")
 	FortuneService fortuneService;
 	
 	public TennisCoach() {
