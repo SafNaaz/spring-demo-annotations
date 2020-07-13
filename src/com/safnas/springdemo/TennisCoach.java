068e7066306819a97933da1c510a1a5b3805a7d0
Safnas <iamsafnas@gmail.com>
2020-07-14 01:17:05 +0530

    resolved using qualifier

--- src/com/safnas/springdemo/TennisCoach.java
+++ src/com/safnas/springdemo/TennisCoach.java
@@ -1,11 +1,13 @@
 package com.safnas.springdemo;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
 public class TennisCoach implements Coach {
 	@Autowired
+	@Qualifier("happyFortuneService")
 	FortuneService fortuneService;
 	
 	public TennisCoach() {
