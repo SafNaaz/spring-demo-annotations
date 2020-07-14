94e4514b5437497f96ca7dd9bd62ae6646f1fd4e
Safnas <iamsafnas@gmail.com>
2020-07-14 20:27:56 +0530

    spring bean config using java

--- src/com/safnas/springdemo/SportConfig.java
+++ src/com/safnas/springdemo/SportConfig.java
@@ -1,10 +1,20 @@
 package com.safnas.springdemo;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ComponentScan("com.safnas.springdemo")
+//@ComponentScan("com.safnas.springdemo")
 public class SportConfig {
-
+	
+	@Bean
+	public FortuneService sadFortuneService(){
+		return new SadFortuneService();
+	}
+	
+	@Bean
+	public Coach swimCoach(){
+		return new SwimCoach(sadFortuneService());
+	}
 }
